3b5977dfb2f8d23592f3906e2963112de4861750
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-09-19 15:57:37 +0200

    modifs header accueil et cdn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
+
+//import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+
+Turbo.session.drive = false
+
+require("@rails/activestorage").start()
+
+//= require jquery_ujs
+//= require jquery
+//= require Chart.min
+
 
