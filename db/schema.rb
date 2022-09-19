34592e909d15c1ea4633d406c5fe25fa78ec87c6
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-09-19 17:40:00 +0200

    ajout clients et produits

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_180551) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_153855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "nom"
+    t.string "mail"
+    t.text "commentaires"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "produits", force: :cascade do |t|
+    t.string "nom"
+    t.decimal "prix"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
