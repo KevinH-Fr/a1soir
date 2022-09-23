a2f2cdd688885b7b3aeac6e9cf032463a1c183a5
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-09-23 12:07:13 +0200

    ajout slideshow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
-        accueil_admin_path() # your path
+        accueil_admin_path() # rediriger apres login
     end
 
 end
