cc0852237b7abaf618adba2f8261c3551e8cb6fa
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-10-03 11:45:44 +0200

    btns show hide avec stimulus

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  static targets = ["dropdownContent", "openButton", "closeButton"]
+
+  connect() {
+    this.dropdownContentTarget.hidden = true
+    this.closeButtonTarget.hidden = true
+    console.log("hello from stiumuls controller")
+  }
+
+  openDropdown() {
+    this.dropdownContentTarget.hidden = false
+    this.openButtonTarget.hidden = true
+    this.closeButtonTarget.hidden = false
+  }
+
+  closeDropdown() {
+    this.dropdownContentTarget.hidden = true
+    this.openButtonTarget.hidden = false
+    this.closeButtonTarget.hidden = true
+  }
+}
