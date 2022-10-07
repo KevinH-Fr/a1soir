830f8c6befa7aa90e72b509dc6f324bbe3b8c868
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-10-07 19:09:50 +0200

    avancement paiement form et redirect

--- app/controllers/paiements_controller.rb
+++ app/controllers/paiements_controller.rb
@@ -9,10 +9,14 @@ class PaiementsController < ApplicationController
   end
 
   def new
-    @paiement = Paiement.new
+    @paiement = Paiement.new paiement_params
+
+    @commandeId = params[:commandeId]
+    session[:commandeId] = params[:commandeId]
   end
 
   def edit
+    @commandeId = params[:commandeId]
   end
 
   def create
@@ -20,7 +24,7 @@ class PaiementsController < ApplicationController
 
     respond_to do |format|
       if @paiement.save
-        format.html { redirect_to paiement_url(@paiement), notice: "Paiement was successfully created." }
+        format.html { redirect_to commande_url(@paiement.commande_id), notice: "Paiement was successfully created." }
         format.json { render :show, status: :created, location: @paiement }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -32,7 +36,7 @@ class PaiementsController < ApplicationController
   def update
     respond_to do |format|
       if @paiement.update(paiement_params)
-        format.html { redirect_to paiement_url(@paiement), notice: "Paiement was successfully updated." }
+        format.html { redirect_to commande_url(@paiement.commande_id), notice: "Paiement was successfully updated." }
         format.json { render :show, status: :ok, location: @paiement }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -45,7 +49,7 @@ class PaiementsController < ApplicationController
     @paiement.destroy
 
     respond_to do |format|
-      format.html { redirect_to paiements_url, notice: "Paiement was successfully destroyed." }
+      format.html { redirect_to commande_url(params[:id]), notice: "Paiement was successfully destroyed." }
       format.json { head :no_content }
     end
   end
