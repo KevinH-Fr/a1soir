7ce429c23432639c462d7fa79ed5e8d461f06cb0
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-10-07 14:25:45 +0200

    commandes lies dans clients

--- app/models/commande.rb
+++ app/models/commande.rb
@@ -2,6 +2,8 @@ class Commande < ApplicationRecord
     belongs_to :client
 
 
+    scope :client_courant, ->  (client_courant) { where("client_id = ?", client_courant)}
+
     def full_name
         "n°#{id} | #{nom}"
     end
