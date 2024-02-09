7056c0636ef4e467cb087b39995b0d523871a9eb
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-02-09 18:50:00 +0100

    correction responsive sur admin dashboard et ajout new path

--- app/helpers/home_admin_helper.rb
+++ app/helpers/home_admin_helper.rb
@@ -1,27 +1,34 @@
 module HomeAdminHelper
-  def dashboard_card(title, path, items, icon)
+  def dashboard_card(title, path, new_path, icon, items)
     content_tag(:div, class: "card m-2") do
-      concat(content_tag(:div, class: "header-bloc-dashboard d-flex justify-content-between align-items-center p-2") do
-        concat(content_tag(:div, class: "format-titre-dashboard text-start") do
-          concat(content_tag(:i, nil, class: "fa iconcolor fa-#{icon} ms-1 me-1"))
-          concat(link_to title, path, class: "format-titre-dashboard text-start text-dark", style: "text-decoration: none;")
-        end)
+      concat(content_tag(:div, class: "bg-dark text-light py-2") do
+        concat(content_tag(:div, class: "row align-items-center m-0 p-0") do
+          concat(content_tag(:div, class: "col text-start ") do
+            concat(content_tag(:i, nil, class: "fa fa-xl brand-colored fa-#{icon} ms-1 me-1"))
+          end)
 
-        concat(content_tag(:div, class: "text-end") do
-          concat(link_to("", new_client_path, class: "fa fa-add add-dashboard btn btn-sm"))
+          concat(content_tag(:div, class: "col text-center fw-bold") do
+            concat(link_to title, path, class: "text-center no-underline")
+          end)
+
+          concat(content_tag(:div, class: "col text-end m-0 p-0") do
+            concat(link_to("", new_path, class: "fa fa-add btn btn-warning btn-sm mx-2"))
+          end)
         end)
       end)
 
-      concat(content_tag(:div, class: "card-body body-bloc-dashboard p-2") do
-        concat(content_tag(:div, class: "row") do
-          items.each do |item|
-            concat(content_tag(:div, class: "col-12 col-md-6") do
-              concat(content_tag(:div, class: "d-flex align-items-center") do
-                concat(content_tag(:i, nil, class: "fa fa-#{item[:icon]} me-1"))
-                concat(link_to item[:text], item[:path], class: "text-dark crop", style: "text-decoration: none;")
+      concat(content_tag(:div, class: "card-body body-bloc-dashboard") do
+        concat(content_tag(:table, class: "table bloc-table table-primary table-striped table-hover m-0 p-0") do
+          concat(content_tag(:tbody, class: "table-light table-dashboard") do
+            items.each do |item|
+              concat(content_tag(:tr) do
+                concat(content_tag(:td) do
+                  concat(content_tag(:i, nil, class: "fa fa-#{item[:icon]} me-1")) # Include icon for each item
+                  concat(link_to item[:text], item[:path], class: "text-dark crop", style: "text-decoration: none;")
+                end)
               end)
-            end)
-          end
+            end
+          end)
         end)
       end)
     end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,6 @@
 class Client < ApplicationRecord
+
+    def full_name
+        prenom + " " + nom
+    end
 end
