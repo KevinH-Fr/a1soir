c1ec26cb6681fd17c72c3dbca8094057f9fa66d6
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-03-06 18:09:41 +0100

    base recup du prix produit vers article

--- app/models/commande.rb
+++ app/models/commande.rb
@@ -14,4 +14,12 @@ class Commande < ApplicationRecord
   def ref_commande
     "C#{ 1000 + id }"
   end
+
+  def is_location
+    location
+  end
+
+  def is_vente
+    location ? false : true 
+  end
 end
