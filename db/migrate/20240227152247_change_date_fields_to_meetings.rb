4bc1b21df43adffaee27c24ea124ea1280fe36ed
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-03-19 12:06:15 +0100

    delete migration time to meetings

--- db/migrate/20240227152247_change_date_fields_to_meetings.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateFieldsToMeetings < ActiveRecord::Migration[7.1]
-  def change
-    change_column :meetings, :datedebut, :time
-    change_column :meetings, :datefin, :time
-  end
-end
