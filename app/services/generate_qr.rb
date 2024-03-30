944e882e34e077577958069ed93fec4d3e0302d8
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-03-30 22:41:55 +0100

    host for generate qr in prod

--- app/services/generate_qr.rb
+++ app/services/generate_qr.rb
@@ -15,7 +15,7 @@ class GenerateQr < ApplicationService
             action: "show",
             id: model.id,
             only_path: false,
-            host: 'localhost:3000', #'a1soir.herokuapp.com',
+            host: ['localhost:3000', 'a1soir-2-2a03802389d6.herokuapp.com'], #'a1soir.herokuapp.com',
             source: 'from_qr')
 
     qrcode = RQRCode::QRCode.new(qr_url)
