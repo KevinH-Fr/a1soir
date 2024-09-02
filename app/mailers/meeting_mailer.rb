413e83665c817dc109dec09ab62efaf5e205703d
capsha1 <kevin.hoffman.france@gmail.com>
2024-09-02 15:29:31 +0200

    update heroku and push schedule meeting reminder

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -3,15 +3,12 @@ class MeetingMailer < ApplicationMailer
   def reminder_email(meeting)
     @meeting = meeting
 
-
       # Determine the recipient based on association
       @recipient = if meeting.commande.present?
       meeting.commande.client
     else
       meeting.client 
     end
-
-    puts "__________recipient email: #{@recipient.mail}___________________"
 
     # You can customize the subject and email body as needed
     mail(to: @recipient.mail, subject: "Rappel RDV à venir#{meeting.datedebut.strftime('%H:%M')}")
