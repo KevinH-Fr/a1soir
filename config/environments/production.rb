fe5f69c8572d4085a88645f90b8bf17c206471a7
capsha1 <kevin.hoffman.france@gmail.com>
2024-09-02 13:43:30 +0200

    mailer for prod with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,21 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = {host:'a1soir-2-2a03802389d6.herokuapp.com', protocol: "https"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5,
+    openssl_verify_mode: 'none'
+  }
+  
+
 end
