3872625ca69876813c4cc9a877e935c8c5e50fe0
capsha1 <kevin.hoffman.france@gmail.com>
2025-02-04 15:51:58 +0100

    deliver now on mail

--- app/jobs/meeting_reminder_job.rb
+++ app/jobs/meeting_reminder_job.rb
@@ -1,21 +1,14 @@
-# app/jobs/meeting_reminder_job.rb
 class MeetingReminderJob < ApplicationJob
   queue_as :default
 
   def perform
-    # Ensure tomorrow's date is interpreted in UTC
     tomorrow_date = Time.zone.tomorrow.to_date
-  
-    # Convert datedebut to UTC before comparing
     meetings = Meeting.where("DATE(datedebut) = ?", tomorrow_date)
-  
+
     puts "_____call meeting reminder job on #{meetings.count} meetings_____________"
-  
-    # Send reminder emails for each meeting
+
     meetings.each do |meeting|
-      MeetingMailer.reminder_email(meeting).deliver_later
+      MeetingMailer.reminder_email(meeting).deliver_now  # Use deliver_now
     end
   end
-  
-  
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 # config/schedule.rb
-every 1.day, at: '6:00 pm', roles: [:app] do
-    runner "MeetingReminderJob.perform_now"
-  end
+# every 1.day, at: '6:00 pm', roles: [:app] do
+#     runner "MeetingReminderJob.perform_now"
+# end
   
