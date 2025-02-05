62e5ec05f3b31e444e1496f4699ebc3ea3978521
capsha1 <kevin.hoffman.france@gmail.com>
2025-02-05 14:59:37 +0100

    adding meeting invite to test

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -7,6 +7,13 @@ class MeetingMailer < ApplicationMailer
     @recipient = meeting.commande&.client || meeting.client
       
     return unless @recipient&.mail
+
+    ics_content = MeetingInviteIcsService.new(@meeting).generate  # Generate .ics for a single meeting
+
+    attachments["meeting-#{@meeting.id}.ics"] = {
+      mime_type: 'text/calendar',
+      content: ics_content
+    }
   
     I18n.locale = @recipient.language || :fr 
     @subject = I18n.t('reminders.subject')
