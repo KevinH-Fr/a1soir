421e66ccd3871aa3dadef04065aa2d7e35ad83f0
capsha1 <kevin.hoffman.france@gmail.com>
2025-02-08 18:28:22 +0100

    fixing etiquettes

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,8 +1,20 @@
 module Public
-  class ApplicationController < ActionController::Base
-    layout "public"
+   class ApplicationController < ActionController::Base
+      layout "public"
 
-   # before_action :authenticate_user!
-  
-  end
+      # before_action :authenticate_user!
+
+      # tempo pour construire en cachant
+      before_action :authenticate_vendeur_or_admin! 
+
+      private
+
+      def authenticate_vendeur_or_admin!
+         unless current_admin_user && (current_admin_user.vendeur? || current_admin_user.admin?)
+            render "admin/home_admin/demande_connexion", alert: "Vous n'avez pas accès à cette page. Veuillez vous connecter."
+         end
+      end
+
+   end 
+   
 end
