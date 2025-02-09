dd66dee74409abacb48da4f6cce5b69729a72462
capsha1 <kevin.hoffman.france@gmail.com>
2025-02-09 23:11:56 +0100

    protect shop subdomain

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,17 +1,16 @@
 module Public
    class ApplicationController < ActionController::Base
       layout "public"
-
       # before_action :authenticate_user!
 
       # tempo pour construire en cachant
-      #before_action :authenticate_vendeur_or_admin! 
+      before_action :authenticate_vendeur_or_admin! 
 
       private
 
       def authenticate_vendeur_or_admin!
          unless current_admin_user && (current_admin_user.vendeur? || current_admin_user.admin?)
-            render "admin/home_admin/demande_connexion", alert: "Vous n'avez pas accès à cette page. Veuillez vous connecter."
+          #  render "public/home_admin/demande_connexion", alert: "Vous n'avez pas accès à cette page. Veuillez vous connecter."
          end
       end
 
