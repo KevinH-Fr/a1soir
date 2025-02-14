45b8950fd49c88fb85d89adbf63118490fbeae1b
capsha1 <kevin.hoffman.france@gmail.com>
2025-02-14 17:51:32 +0100

    fix distinct on handle

--- app/controllers/public/pages_controller.rb
+++ app/controllers/public/pages_controller.rb
@@ -12,7 +12,8 @@ module Public
     def categorie
       @categorie = CategorieProduit.find(params[:id])
       @produits = @categorie.produits
-      @produits_uniques = @produits.group(:handle)
+      # Group produits by handle and pick the first product for each unique handle
+      @produits_uniques = @produits.group_by(&:handle).map { |_, produits| produits.first }
 
       # Get all associated tailles, remove nil values, and ensure uniqueness
       @tailles = @produits.map { |produit| produit.taille }.compact.uniq
