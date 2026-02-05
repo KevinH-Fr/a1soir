a542b7e3902fd50305ea9bee39c2d4c75485a8f2
capsha1 <kevin.hoffman.france@gmail.com>
2026-02-05 19:04:08 +0100

    filter frs on NA

--- app/helpers/produits_filters_helper.rb
+++ app/helpers/produits_filters_helper.rb
@@ -8,7 +8,7 @@ module ProduitsFiltersHelper
     selected_label =
     if selected_value.present? && model
       case param_key
-      when :filter_taille, :filter_categorie, :filter_couleur
+      when :filter_taille, :filter_categorie, :filter_couleur, :filter_fournisseur
         selected_value == "na" ? "NA" : model.find_by(id: selected_value)&.nom
       else
         model.find_by(id: selected_value)&.nom
@@ -81,7 +81,7 @@ module ProduitsFiltersHelper
               end
             )
           
-            if [:filter_taille, :filter_categorie, :filter_couleur, :filter_statut].include?(param_key)
+            if [:filter_taille, :filter_categorie, :filter_couleur, :filter_statut, :filter_fournisseur].include?(param_key)
               concat(
                 content_tag(:li) do
                   link_to(
