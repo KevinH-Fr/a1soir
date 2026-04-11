dea9fe7296712f0550d7d09176f2407c49646cdb
capsha1 <kevin.hoffman.france@gmail.com>
2026-04-11 19:50:03 +0200

    styling and paginate stripe payments on admin

--- app/controllers/admin/stripe_payments_controller.rb
+++ app/controllers/admin/stripe_payments_controller.rb
@@ -2,7 +2,13 @@
 
 class Admin::StripePaymentsController < Admin::ApplicationController
   def index
+    @stripe_payments_count = StripePayment.count
     payments = StripePayment.order(created_at: :desc).includes(:commande, stripe_payment_items: :produit)
-    @pagy, @stripe_payments = pagy(payments, items: 25)
+    @pagy, @stripe_payments = pagy_countless(payments, items: 25)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 end
